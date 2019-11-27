dc19f5a0310846e197d6b69d8f25594c6f77d2ef
Alessio Milanese <milanese.alessio@gmail.com>
2019-11-27 14:00:03 +0100

    add function to convert the alignment ino 1-hot encoding

--- bin/align.py
+++ bin/align.py
@@ -51,3 +51,33 @@ def merged_fasta(filein):
     # give back the last sequence
     if seq != "":
         yield seq
+
+# ------------------------------------------------------------------------------
+# function to convert the nucleotide alignment into 1-hot encoding.
+# Note that we select only the nucleotides that corresponds to the inner state
+# of the HMM.
+encoding_dic = {
+               "A":"00001",
+               "C":"00010",
+               "G":"00100",
+               "T":"01000",
+               "U":"01000",
+               "others":"10000"
+               }
+def convert_alignment(merged_fasta,verbose):
+    converted_ali = merged_fasta.split("\t")[0]+"\t"
+    for i in merged_fasta.split("\t")[1]:
+        # 1-hot encoding
+        # the ACGTU are converted, everything else that is upper case, is considered
+        # as a gap ('-').
+        # for example also 'N' is converted to "-" -> "1,0,0,0,0"
+        # Note that the upper case letters and "-" represents alignment to the
+        # hidden state of the HMM.
+        i_c = ""
+        if not i.islower():
+            if i in encoding_dic:
+                i_c = encoding_dic[i]
+            else:
+                i_c = encoding_dic["others"]
+        converted_ali = converted_ali + i_c
+    return converted_ali
