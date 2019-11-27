0db00b8eca200a04e87c0d597e1014f7deaf9e34
Alessio Milanese <milanese.alessio@gmail.com>
2019-11-27 13:59:18 +0100

    add function to merge fasta files across different lines

--- bin/align.py
+++ bin/align.py
@@ -26,3 +26,28 @@ def is_tool(name):
         if e.errno == errno.ENOENT:
             return False
     return True
+
+# ------------------------------------------------------------------------------
+# function to convert a fasta file with multiple lines into a one line separated
+# by a "\t"
+# Example:
+# >test_fasta_header
+# ATTGCGATTTCT
+# CGGTATCGGTAT
+# CGGTTA
+### TO:
+# >test_fasta_header\tATTGCGATTTCTCGGTATCGGTATCGGTTA
+def merged_fasta(filein):
+    # filein is a stream of data (from hmmalign)
+    seq = ""
+    for line_b in filein:
+        line = line_b.decode("utf-8").rstrip()
+        if line.startswith(">"):
+            if seq != "":
+                yield seq
+            seq = line+"\t"
+        else:
+            seq = seq + line
+    # give back the last sequence
+    if seq != "":
+        yield seq
