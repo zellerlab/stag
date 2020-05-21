d95b0993c97faa3ab8b0ed36e642da402d879ca1
Alessio Milanese <milanese.alessio@gmail.com>
2020-05-21 19:43:42 +0200

    add functions to check that a tool exists

--- test.py
+++ test.py
@@ -19,3 +19,32 @@ path_mOTUs = os.path.realpath(__file__)
 path_array = path_mOTUs.split("/")
 relative_path = "/".join(path_array[0:-1])
 relative_path = relative_path + "/"
+
+
+# ------------------------------------------------------------------------------
+# function to check if a specific tool exists
+# ------------------------------------------------------------------------------
+def is_tool_and_return0(name):
+    try:
+        devnull = open(os.devnull)
+        popenCMD = shlex.split(name)
+        child = subprocess.Popen(popenCMD, stdout=devnull, stderr=devnull)
+        streamdata = child.communicate()
+        rc = child.wait()
+        if rc == 0:
+            return True
+        else:
+            return False
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            return False
+    return True
+
+def is_tool(name):
+    try:
+        devnull = open(os.devnull)
+        subprocess.Popen([name], stdout=devnull, stderr=devnull).communicate()
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            return False
+    return True
