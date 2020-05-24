a342c449c9fc4e36c13964bb9831fe5add09afe1
Alessio Milanese <milanese.alessio@gmail.com>
2020-05-24 18:03:58 +0200

    zip and unzip the database

--- bin/classify_genome.py
+++ bin/classify_genome.py
@@ -12,13 +12,29 @@ import shutil
 import subprocess
 import shlex
 
+# ==============================================================================
+# UNZIP THE DATABASE
+# ==============================================================================
+def load_genome_DB(database, tool_version, verbose):
+    dirpath = tempfile.mkdtemp()
+    shutil.unpack_archive(database, dirpath,"gztar")
+    list_files = [f for f in os.listdir(dirpath) if os.path.isfile(os.path.join(dirpath, f))]
+    return list_files,dirpath
 
 #===============================================================================
 #                                      MAIN
 #===============================================================================
 def classify_genome(database, fasta_input, verbose, threads, output, long_out, tool_version):
     # FIRST: unzip the database
+    database_files, temp_dir = load_genome_DB(database, tool_version, verbose)
+
     # SECOND: run prodigal on the fasta genome
+
     # THIRD: find the marker genes from the predicted genes
+
     # FOURTH: classify the marker genes
+
+    # we remove the temp dir
+    shutil.rmtree(temp_dir)
+
     # FIFTH: join prediction
