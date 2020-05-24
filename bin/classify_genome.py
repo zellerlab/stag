c4b698e6dda0a90a0bda1007dbb698957c82013c
Alessio Milanese <milanese.alessio@gmail.com>
2020-05-24 18:22:50 +0200

    add -D option

--- bin/classify_genome.py
+++ bin/classify_genome.py
@@ -21,14 +21,23 @@ def load_genome_DB(database, tool_version, verbose):
     list_files = [f for f in os.listdir(dirpath) if os.path.isfile(os.path.join(dirpath, f))]
     return list_files,dirpath
 
+# ==============================================================================
+# RUN PRODIGAL
+# ==============================================================================
+# run prodigal on all the genomes listed in fasta_input
+def run_prodigal_genomes(genomes_file_list, verbose):
+    return 1
+
+
 #===============================================================================
 #                                      MAIN
 #===============================================================================
-def classify_genome(database, fasta_input, verbose, threads, output, long_out, tool_version):
+def classify_genome(database, genomes_file_list, verbose, threads, output, long_out, tool_version):
     # FIRST: unzip the database
     database_files, temp_dir = load_genome_DB(database, tool_version, verbose)
 
     # SECOND: run prodigal on the fasta genome
+    run_prodigal_genomes(genomes_file_list, verbose)
 
     # THIRD: find the marker genes from the predicted genes
 
