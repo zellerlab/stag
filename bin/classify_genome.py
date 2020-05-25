195ae6580fde8ce41c8d94b542e005fa7a7f2387
Alessio Milanese <milanese.alessio@gmail.com>
2020-05-25 22:40:10 +0200

    add code to run prodigal

--- bin/classify_genome.py
+++ bin/classify_genome.py
@@ -11,6 +11,25 @@ import tempfile
 import shutil
 import subprocess
 import shlex
+import errno
+
+# ------------------------------------------------------------------------------
+# function to check if a specific tool exists
+def is_tool(name):
+    try:
+        devnull = open(os.devnull)
+        subprocess.Popen([name], stdout=devnull, stderr=devnull).communicate()
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            return False
+    return True
+
+# ------------------------------------------------------------------------------
+# dev null
+try:
+    from subprocess import DEVNULL
+except ImportError:
+    DEVNULL = open(os.devnull, 'wb')
 
 # ==============================================================================
 # UNZIP THE DATABASE
@@ -24,9 +43,40 @@ def load_genome_DB(database, tool_version, verbose):
 # ==============================================================================
 # RUN PRODIGAL
 # ==============================================================================
+# run prodigal on one genome
+def run_prodigal(genome, verbose):
+    # we need two files, one for the proteins and one for the genes
+    genes = tempfile.NamedTemporaryFile(delete=False, mode="w")
+    proteins = tempfile.NamedTemporaryFile(delete=False, mode="w")
+    # prodigal command
+    prodigal_command = "prodigal -i "+genome+" -d "+genes.name
+    prodigal_command = prodigal_command + " -a "+proteins.name
+    # run prodigal
+    if not is_tool("prodigal"):
+        sys.stderr.write("[E::align] Error: prodigal is not in the path.\n")
+        sys.exit(1)
+    CMD2 = shlex.split(prodigal_command)
+    parse_cmd = subprocess.Popen(CMD2, stdout=DEVNULL,stderr=subprocess.PIPE)
+    # we save stderr if necessary
+    all_stderr = ""
+    for line in parse_cmd.stderr:
+        #filter lines
+        line = line.decode('ascii')
+        all_stderr = all_stderr + line
+    return_code = parse_cmd.wait()
+    if return_code:
+        sys.stderr.write("[E::align] Error. prodigal failed\n\n")
+        sys.stderr.write(all_stderr)
+        sys.exit(1)
+
+    return [genes.name, proteins.name]
+
 # run prodigal on all the genomes listed in fasta_input
 def run_prodigal_genomes(genomes_file_list, verbose):
-    return 1
+    result = dict()
+    for g in genomes_file_list:
+        result[g] = run_prodigal(g, verbose)
+    return result
 
 
 #===============================================================================
@@ -37,7 +87,11 @@ def classify_genome(database, genomes_file_list, verbose, threads, output, long_
     database_files, temp_dir = load_genome_DB(database, tool_version, verbose)
 
     # SECOND: run prodigal on the fasta genome
-    run_prodigal_genomes(genomes_file_list, verbose)
+    genomes_pred = run_prodigal_genomes(genomes_file_list, verbose)
+    # genomes_pred is a dictionary where the keys are the genome paths and the
+    # values are lists. First value of the list is the path to the gene file and
+    # second the path to the protein file
+    print(genomes_pred)
 
     # THIRD: find the marker genes from the predicted genes
 
