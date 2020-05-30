c7fe50886bcc37424d6624692239835b72216687
Alessio Milanese <milanese.alessio@gmail.com>
2020-05-30 17:05:35 +0200

    load thresholds when classifing

--- bin/classify_genome.py
+++ bin/classify_genome.py
@@ -40,7 +40,20 @@ def load_genome_DB(database, tool_version, verbose):
     dirpath = tempfile.mkdtemp()
     shutil.unpack_archive(database, dirpath,"gztar")
     list_files = [f for f in os.listdir(dirpath) if os.path.isfile(os.path.join(dirpath, f))]
-    return list_files,dirpath
+    # there must be a file with the name 'threshold_file.tsv'
+    if not "threshold_file.tsv" in list_files:
+        sys.stderr.write("[E::align] Error: threshold_file.tsv is missing.\n")
+        sys.exit(1)
+    # we load the thresholds
+    gene_thresholds = dict()
+    o = open(os.path.join(dirpath, "threshold_file.tsv"))
+    for line in o:
+        vals = line.rstrip().split("\t")
+        gene_thresholds[vals[0]] = vals[1]
+    o.close()
+    # we remove the threshold file from the list of genes
+    list_files.remove("threshold_file.tsv")
+    return list_files,dirpath,gene_thresholds
 
 # ==============================================================================
 # RUN PRODIGAL
@@ -218,7 +231,7 @@ def fetch_MGs(database_files, database_path, genomes_pred, keep_all_genes):
 #===============================================================================
 def classify_genome(database, genomes_file_list, verbose, threads, output, long_out, tool_version, keep_all_genes):
     # FIRST: unzip the database
-    database_files, temp_dir = load_genome_DB(database, tool_version, verbose)
+    database_files, temp_dir, gene_thresholds = load_genome_DB(database, tool_version, verbose)
 
     # SECOND: run prodigal on the fasta genome
     genomes_pred = run_prodigal_genomes(genomes_file_list, verbose)
