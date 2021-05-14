fe26093da52c553358c4f70b7a6bd2b38e968d62
Alessio Milanese <milanese.alessio@gmail.com>
2021-05-14 15:33:07 +0200

    add function to find centroids

--- stag/train_NN.py
+++ stag/train_NN.py
@@ -94,7 +94,77 @@ def estimate_weights(ALI, tax, sel_level):
 # centorids vs all to identify the threshold for genus and species (if for
 # example the NN_start_level is family)
 
-def find_trhresholds(all_transformed, tax):
+# ------------------------------------------------------------------------------
+# Calculate perc identity between two sequences
+#  - ALI is the pandas DF
+#  - pos1 and pos2 are part of the rownames of ALI
+def perc_id_vectors(ALI,pos1,pos2):
+    seq1 = ALI.loc[pos1,].to_numpy()
+    seq2 = ALI.loc[pos2,].to_numpy()
+    # euclidean distance
+    dist = np.linalg.norm(seq1-seq2)
+    return dist   ##### now we return a distance!!!!!!!!!!
+
+
+
+# ------------------------------------------------------------------------------
+# FIND CENTROIDS
+# It will return a dictionary where the keys are the centroid sequences and the
+# value is the species
+def find_centroids(ALI,tax):
+    # first find all species and their map to the seq id
+    all_species = dict()
+    for seq in ALI.index.values:
+        if not tax[seq][-1] in all_species:
+            all_species[tax[seq][-1]] = list()
+        all_species[tax[seq][-1]].append(seq)
+    # now we go by species and find the centroid
+    result = dict()
+    for species in all_species:
+        # if there is only one
+        if len(all_species[species]) == 1:
+            result[all_species[species][0]] = species
+            continue
+        # if there are 2, we choose randomly
+        if len(all_species[species]) == 2:
+            result[all_species[species][0]] = species
+            continue
+        # if there are at least 3, then we calculate the centroid
+        all_id_dist = dict()
+        for id in all_species[species]:
+            all_id_dist[id] = 0
+        for i in range(len(all_species[species])-1):
+            for j in range(i+1,len(all_species[species])):
+                id_i = all_species[species][i]
+                id_j = all_species[species][j]
+                # find vector and distance
+                perc_id = perc_id_vectors(ALI,id_i,id_j)
+                # add distance
+                all_id_dist[id_i] = all_id_dist[id_i] + perc_id
+                all_id_dist[id_j] = all_id_dist[id_j] + perc_id
+        # now we need to identify the minimum distance (i.e. centroid)
+        sel_min = ""
+        val_min = float('inf')
+        for i in all_id_dist:
+            if all_id_dist[i] < val_min:
+                val_min = all_id_dist[i]
+                sel_min = i
+        # we add it
+        result[sel_min] = species
+
+    # the result is a ditionary where the values are:
+    # gene1 -> "E.coli"
+    # gene236 -> "P.copri"
+
+    # return the result
+    return result
+
+def find_thresholds(all_transformed, tax):
+    # find centroids per species
+    for clade in all_transformed:
+        # find centroids per species
+        centroids_this = find_centroids(all_transformed[clade],tax)
+        
     return "dummy1", "dummy2"
 
 #===============================================================================
@@ -107,6 +177,6 @@ def train_NN_classifiers(alignment, tax_file, NN_start_level):
     all_LMNN, all_transformed = estimate_weights(alignment, tax, NN_start_level)
 
     # 2. find centroids and find the threshold distances
-    thresholds_NN, centroid_seq = find_trhresholds(all_transformed, tax)
+    thresholds_NN, centroid_seq = find_thresholds(all_transformed, tax)
 
     return all_LMNN, thresholds_NN, centroid_seq
