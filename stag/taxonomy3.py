1400133683f1448cd8705a9961d409b5421c1797
Christian Schudoma <christian.schudoma@embl.de>
2021-07-26 17:25:44 +0200

    find_gene_ids will now return genes again; get_last_level_to_genes will now only return leaf nodes

--- stag/taxonomy3.py
+++ stag/taxonomy3.py
@@ -54,26 +54,39 @@ class Taxonomy(dict):
     def copy(self):
         from copy import deepcopy
         return deepcopy(self)
+
     def extract_full_tax_from_gene(self, gene):
         return self.gene_lineages.get(gene)
+
     def get_n_levels(self):
         return self.n_taxlevels
+
     def get_root(self):
         return self.TREE_ROOT
+
     def find_children_node(self, node):
         return list(self.get(node, Taxon()).children.keys())
+
     def get_last_level_to_genes(self):
-        return {node: set(node.genes) for node in self.values()}
+        return {node: set(node.genes) for node in self.values() if node.genes}
+
     def is_last_node(self, node):
         return self.get(node, Taxon()).is_leaf()
+
     def find_gene_ids(self, node=None):
-        
-        node = self[node] if node else list(self[self.TREE_ROOT].children.values())[0]
-        if node.level == self.n_taxlevels - 1:
-            return list(node.genes)
+        nodes = list(self[Taxonomy.TREE_ROOT].children.keys()) if (node is None or node == self.get_root()) else [node]
+
+        if any(self.get(node) is None for node in nodes):
+            raise ValueError(f"Nodelist invalid: {nodes}")
+
         genes = set()
-        for desc in node.species_nodes:
-            genes.update(self.get(desc, Taxon()).genes)
+        for node in nodes:
+            if self[node].level == self.n_taxlevels - 1:
+                genes.update(self[node].genes)
+            else:
+                for descendant in self[node].species_nodes:
+                    genes.update(self.get(descendant, Taxon()).genes)
+
         return list(genes)
 
     def find_gene_ids_old(self, node=None):
@@ -84,6 +97,7 @@ class Taxonomy(dict):
             nodes.extend(node.children.values())
             genes.update(node.genes)
         return list(genes)
+
     def remove_clades(self, nodes):
         removed_genes = set()   
         for node in nodes:
@@ -95,11 +109,11 @@ class Taxonomy(dict):
                 if node2.parent:
                     node2.parent.children.pop(node2.label, None)
                     self._clean_branch(node2.parent)
-                    
                 self.pop(node2.label, None)
         for gene in removed_genes:
             self.gene_lineages.pop(gene, None)
         return list(removed_genes)
+
     def _clean_branch(self, node):
         while True:
             if node.children or not node.parent:
@@ -110,6 +124,7 @@ class Taxonomy(dict):
             except:
                 pass
             node = node.parent
+
     def remove_genes(self, genes):
         empty_nodes = set()
         for gene in genes:
@@ -118,9 +133,10 @@ class Taxonomy(dict):
             if not node.genes:
                 empty_nodes.add(node.label)
         self.remove_clades(empty_nodes)
+
     def find_node_level(self, tax_level):
         nodes = dict()
-        queue = [(self[self.TREE_ROOT], 0)]
+        queue = [(self[self.TREE_ROOT], -1)]
         while queue:
             node, level = queue.pop(0)
             if level + 1 == tax_level:
@@ -129,6 +145,7 @@ class Taxonomy(dict):
             else:
                 queue.extend((child, level + 1) for child in node.children.values())
         return nodes
+
     def get_all_nodes(self, mode=None, get_root=False):
         queue = [(self[self.TREE_ROOT], set())]
         while queue:
@@ -140,6 +157,7 @@ class Taxonomy(dict):
             for child in children:
                 siblings = children.difference({child})
                 queue.append((self[child], siblings))
+
     def ensure_geneset_consistency(self, genes):
         genes_in_tree = set(self.find_gene_ids())
         logging.info(f"   CHECK: genes in geneset: {len(genes)}")
