815b3dc5f70fc147aae2b1f5856c143eee37f6bb
Alessio Milanese <milanese.alessio@gmail.com>
2021-11-20 08:41:28 +0100

    correct error when saving distances with -M

--- stag/train_NN.py
+++ stag/train_NN.py
@@ -43,7 +43,7 @@ def load_tax_line(tax_file, ALI):
 #  6:[0.3,0.6,0.5,1.2,1.0]}
 # where 5 means genus
 def save_distances_to_file(dist_all_vs_centroids, clade, intermediate_dist_for_NN):
-    o = open(intermediate_dist_for_NN,"r")
+    o = open(intermediate_dist_for_NN,"w")
     for dis in distances:
         o.write(clade+"\t"+str(dis))
         for i in distances[dis]:
