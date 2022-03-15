373754560a1940deaf69f6c0842ff52ab0da9b50
Christian Schudoma <christian.schudoma@embl.de>
2022-03-15 15:01:18 +0100

    added drop_duplicates-flag; disabled unpack-logging

--- stag/alignment.py
+++ stag/alignment.py
@@ -57,12 +57,12 @@ class EncodedAlignment:
 	# https://stackoverflow.com/questions/22227595/convert-integer-to-binary-array-with-suitable-padding
 	# https://stackoverflow.com/questions/45604688/apply-function-on-each-row-row-wise-of-a-numpy-array
 	# https://pypi.org/project/bitarray/ <- another possibility
-	def __init__(self, fn=None, other_aln=None, ncols=None, npads=None):
+	def __init__(self, fn=None, other_aln=None, ncols=None, npads=None, drop_duplicates=True):
 		self.alignment = None
 		self.ncols = None
 		self.npads = None
 		if fn:
-			self._load_alignment(fn)
+			self._load_alignment(fn, drop_duplicates=drop_duplicates)
 		elif other_aln is not None:
 			self.alignment = other_aln
 			self.ncols = ncols
@@ -75,13 +75,14 @@ class EncodedAlignment:
 			npads=self.npads
 		)
 
-	def _load_alignment(self, fn):
+	def _load_alignment(self, fn, drop_duplicates=True):
 		with open(fn) as _in:
 			self.ncols, self.npads = map(int, next(_in).strip().split("\t"))
 			self.alignment = pd.read_csv(_in, header=None, index_col=0, sep="\t")
 			logging.info(f'   LOAD_AL: Number of genes: {len(list(self.alignment.index.values))}')
-			self.alignment = self.alignment.drop_duplicates()
-			logging.info(f'   LOAD_AL: Number of genes, after removing duplicates: {len(list(self.alignment.index.values))}')
+			if drop_duplicates:
+				self.alignment = self.alignment.drop_duplicates()
+				logging.info(f'   LOAD_AL: Number of genes, after removing duplicates: {len(list(self.alignment.index.values))}')
 
 	def get_index(self):
 		return list(self.alignment.index.values)
@@ -154,16 +155,16 @@ class EncodedAlignment:
 		return [negative_candidates[i] for i in clade_indices]
 
 	def get_rows(self, node, rows):	
-		logging.info(f"Unpacking: {node} {len(rows)} rows...")
-		t0 = time.time()
+		# logging.info(f"Unpacking: {node} {len(rows)} rows...")
+		# t0 = time.time()
 
-		subset = self.alignment.loc[rows, : ].to_numpy()
+		# subset = self.alignment.loc[rows, : ].to_numpy()
 		subset = np.apply_along_axis(
 			lambda x: (((x[:, None] & (1 << np.arange(32))[::-1])) > 0).flatten()[:-self.npads],
 			1,
-			subset
+			self.alignment.loc[rows, : ].to_numpy()
 		)
-		logging.info(f"Unpacked {node}: {len(rows)} rows in {time.time() - t0:.3f}s.")
+		# logging.info(f"Unpacked {node}: {len(rows)} rows in {time.time() - t0:.3f}s.")
 
 		return subset
 
